313a070feee44c7ee72bc16812dd540146192360
aaajii <jwpbanawan@gmail.com>
2022-07-07 21:58:46 +0800

    Initialize from template

---
+++ src/components/layout.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div className="p-4 pt-0">
+        <main>{children}</main>
+      </div>
+    </>
+  )
+}
+
+export default Layout
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const IndexPage = () => (
+  <Layout>
+    Gatsby Home
+  </Layout>
+)
+
+export default IndexPage
