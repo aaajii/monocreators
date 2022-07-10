78ede07147e29720573567cb5605347ab92a0e6c
aaajii <jwpbanawan@gmail.com>
2022-07-10 20:12:14 +0800

    Add Artist list content

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -1,5 +1,23 @@
-function Artists({people}) {
-    return ( "Artists Section" );
+import React from "react"
+
+function Artists({ people }) {
+  return (
+    <div className="p-12">
+      {Object.keys(people).map(group => (
+        <>
+          <div className="m-8">
+            <h1 className="text-orange-400 font-bold">{group}</h1>
+            <div className="flex flex-wrap my-8">
+              {people[group].map(person => (
+                <span className="mx-12 first:ml-0 text-lg">{person}</span>
+              ))}
+            </div>
+          </div>
+          <hr />
+        </>
+      ))}
+    </div>
+  )
 }
 
-export default Artists;
+export default Artists
