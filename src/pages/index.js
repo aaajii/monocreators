1f199f91782f3c7b924d9c44f82b1d40ed2f87a7
aaajii <jwpbanawan@gmail.com>
2022-07-10 20:11:51 +0800

    Set up data and main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,52 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
+import { useQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Hero from '../components/Hero'
-import Highlights from '../components/Highlights'
-import Artists from '../components/Artists'
+import Hero from "../components/Hero"
+import Highlights from "../components/Highlights"
+import Artists from "../components/Artists"
 
-const IndexPage = () => { 
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-  
-  return(
-  <Layout>
-    {/* Hero */}
-    <Hero />
-    {/* Highlights */}
-    <Highlights list={data}/>
-    {/* Artists */}
-    <Artists people={data}/>
-  </Layout>
-)}
+const IndexPage = () => {
+  const { list, people } = {
+    list: [
+      {
+        imageUrl: "https://picsum.photos/200",
+        fullName: "Joji",
+      },
+      {
+        imageUrl: "https://picsum.photos/200",
+        fullName: "Rich Brian",
+      },
+      {
+        imageUrl: "https://picsum.photos/200",
+        fullName: "Adie",
+      },
+      {
+        imageUrl: "https://picsum.photos/200",
+        fullName: "Beabadoobie",
+      },
+      {
+        imageUrl: "https://picsum.photos/200",
+        fullName: "Niki",
+      },
+    ],
+    people: {
+      A: ["Adam Healy", "Artificial Intelligence", "Another One"],
+      B: ["Benedict Cucumber", "Billie Jeans", "Bilbo Boggins"],
+      C: ["Chester Cee", "Cola Coca", "Creative Freedom"],
+    },
+  }
+
+  return (
+    <Layout>
+      {/* Hero */}
+      <Hero />
+      {/* Highlights */}
+      <Highlights list={list} />
+      {/* Artists */}
+      <Artists people={people} />
+    </Layout>
+  )
+}
 
 export default IndexPage
